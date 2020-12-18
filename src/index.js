526819504f15d1bfbc31ac33daac30035c412d1a
Jerome Ramos <ramos.jeromeh@gmail.com>
2020-12-18 08:27:53 +0800

    Published by publish-to-git 884989919b4f56e1e3beabde155f03159496beb8 (HEAD -> refs/heads/feature/toggle-parent-row, refs/remotes/origin/master, refs/remotes/origin/HEAD, refs/heads/master) Merge pull request #2 from nod-engineering/fix/delete-row

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import insertTable from "./commands/insertTable";
+import insertBelow from "./commands/insertBelow";
+import insertRight from "./commands/insertRight";
+import removeTable from "./commands/removeTable";
+import removeColumn from "./commands/removeColumn";
+import removeRow from "./commands/removeRow";
+import mergeSelection from "./commands/mergeSelection";
+import splitCell from "./commands/splitCell";
+import checker from "./commands/checker";
+import toggleHeader from "./commands/toggleHeader";
+import toggleParentRow from "./commands/toggleParentRow";
+
+import withTable from "./withTable";
+const commands = {
+  insertTable,
+  insertBelow,
+  insertRight,
+  removeTable,
+  removeColumn,
+  removeRow,
+  mergeSelection,
+  splitCell,
+  checker,
+  toggleHeader,
+  toggleParentRow,
+};
+export { commands, withTable };
