de2f8ecb79a660b6fe418d9fc72546574d631859
Jerome Ramos <ramos.jeromeh@gmail.com>
2021-01-07 10:35:34 +0800

    Published by publish-to-git 965315dc2808cea29372aa1e459b2b996d85d39e (HEAD -> refs/heads/feature/toggle-parent-row, refs/remotes/origin/feature/toggle-parent-row) Add toggle parent row command

---
+++ src/commands/toggleParentRow.js
@@ -0,0 +1,27 @@
+import { Transforms, Editor } from "slate";
+import { get } from "../utils";
+
+const toggleParentRow = ({ table, editor, selected }) => {
+  const { selection } = editor;
+  if (!table || !selection) return;
+  const nodes = Editor.nodes(editor, {
+    at: table[1],
+    match: (n) => n.type === "table_row",
+  });
+
+  for (const node of nodes) {
+    const [rows, path] = node;
+    if (!get(rows, "children", []).length) return;
+    for (const cell of rows.children) {
+      if (get(cell, "selectedCell")) {
+        if (get(rows, "parent")) {
+          Transforms.setNodes(editor, { subParent: selected }, { at: path });
+        } else {
+          Transforms.setNodes(editor, { parent: selected }, { at: path });
+        }
+      }
+    }
+  }
+};
+
+export default toggleParentRow;
